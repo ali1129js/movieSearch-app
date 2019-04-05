d66ee6aee4f648b6b2146725bbf6ab3bbfadd070
ali1129js <ali1129js@gmail.com>
2019-04-05 18:40:08 +0200

    modal works

--- src/Components/MyModal.js
+++ src/Components/MyModal.js
@@ -2,22 +2,17 @@
  * @Author: Ali
  * @Date:   2019-03-29T13:28:37+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-03-30T07:25:48+01:00
+ * @Last modified time: 2019-04-05T18:25:02+02:00
  */
 import React from "react";
-
 import styled from "styled-components";
 const Content = styled.div`
-  background: linear-gradient(to bottom right, #3498db, papayawhip, #f39c12);
+  background: linear-gradient(to bottom right, #3498db, papayawhip, #8e44ad);
   margin: auto;
-  height: 22em;
+  height: 28em;
   width: 32em;
   font-size: 1.2em;
   text-align: center;
-  border: 3px solid #fab1a0;
-  padding: 5px;
-  padding-top: 10px;
-  box-shadow: 8px 10px #7f8c8d;
 `;
 const backdropStyle = {
   position: "fixed",
@@ -25,7 +20,7 @@ const backdropStyle = {
   bottom: 0,
   left: 0,
   right: 0,
-  backgroundColor: "#2d3436",
+  backgroundColor: "#3498db",
   padding: 50
 };
 const MyModal = props => {
@@ -35,16 +30,18 @@ const MyModal = props => {
   return (
     <div className="backdrop" style={backdropStyle}>
       <div className="modal-body">
+        <div className="header">
+          <button className="btn btn-outline-dark" onClick={props.onClose}>
+            Close
+          </button>
+        </div>
         <Content>
-          clcked
-          <div className="footer">
-            <button
-              className="btn btn-outline-dark"
-              onClick={this.props.onClose}
-            >
-              Close
-            </button>
-          </div>
+          {props.title}
+          <p> {props.overview} </p>
+          <img
+            src={`https://image.tmdb.org/t/p/original/${props.backdrop}`}
+            alt={props.title}
+          />
         </Content>
       </div>
     </div>
--- src/Components/TitleComponent.js
+++ src/Components/TitleComponent.js
@@ -2,16 +2,26 @@
  * @Author: Ali
  * @Date:   2019-03-27T11:49:59+01:00
  * @Last modified by:   Ali
- * @Last modified time: 2019-04-05T17:36:22+02:00
+ * @Last modified time: 2019-04-05T18:10:08+02:00
  */
-import React from "react";
+import React, { useState } from "react";
 import MyModal from "./MyModal";
 const TitleComponent = props => {
+  const [isOpen, setModal] = useState(false);
   const handleClicked = () => {
-    console.log("clicked");
+    setModal(!isOpen);
   };
 
-  return (
+  return isOpen ? (
+    <MyModal
+      show={isOpen}
+      onClose={handleClicked}
+      title={props.title}
+      votes={props.votes}
+      overview={props.overview}
+      backdrop={props.backdrop}
+    />
+  ) : (
     <div className="item" onClick={handleClicked}>
       <img
         src={`https://image.tmdb.org/t/p/w500/${props.poster}`}
