69b4a1c620b9bf5b938d2d57b64a407187e9b660
ali1129js <ali1129js@gmail.com>
2019-07-14 04:13:22 +0200

    Updates

---
+++ precache-manifest.c92d5551744abec04e328e65ead183b9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d672aba2a809923d531",
+    "url": "/movieSearch-app/static/js/runtime~main.6d672aba.js"
+  },
+  {
+    "revision": "bdbbaf99907579985986",
+    "url": "/movieSearch-app/static/js/main.bdbbaf99.chunk.js"
+  },
+  {
+    "revision": "2ed6f39df07eb94d4d10",
+    "url": "/movieSearch-app/static/js/1.2ed6f39d.chunk.js"
+  },
+  {
+    "revision": "bdbbaf99907579985986",
+    "url": "/movieSearch-app/static/css/main.657bc689.chunk.css"
+  },
+  {
+    "revision": "2ed6f39df07eb94d4d10",
+    "url": "/movieSearch-app/static/css/1.37b7b5d6.chunk.css"
+  },
+  {
+    "revision": "b0f70ed75f3dae3ea8d2db938954dabd",
+    "url": "/movieSearch-app/index.html"
+  }
+];
